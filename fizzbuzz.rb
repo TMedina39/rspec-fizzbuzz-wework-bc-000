90dd0cfc38e09afbbcdc5c6b7875dccea704276e
Tomas Medina <tomas.medina@wework.com>
2018-02-11 19:58:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,13 @@
 def fizzbuzz(int)
 
   if int % 3 == 0 && int % 5 == 0
-      #"Fizz"
-      #"Buzz"
       "FizzBuzz"
-      puts "FizzBuzz"
-  end
   elsif int % 3 == 0
       "Fizz"
-      puts "Fizz"
-  end
+  elsif int % 5 == 0
+      "Buzz"
+  else
+       nil
 end
 fizzbuzz(3)
 fizzbuzz(5)
